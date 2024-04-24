0811050034732489ea80d3ecf5f449e57d5bfb86
unknown <milenamelo1709@gmail.com>
2024-04-23 22:10:48 -0300

    first commit

--- App.js
+++ App.js
@@ -4,7 +4,10 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.title}>
+        Milena 
+        Hello word!
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +16,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'white',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title:{
+  }
 });
+
