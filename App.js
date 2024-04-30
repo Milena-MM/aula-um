1c8924b711d9f6886821ffcd0c012d1166506449
unknown <milenamelo1709@gmail.com>
2024-04-29 22:16:52 -0300

    Fe feat: adiconando styleSheets

--- App.js
+++ App.js
@@ -1,26 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {Text, View} from 'react-native';
+import {styles} from './src/class/stygit '
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>
-        Milena 
-        Hello word!
-      </Text>
-      <StatusBar style="auto" />
+      <Text style={styles.texto}>Hello</Text>
+      <Text style={styles.texto}>World</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'white',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title:{
-  }
-});
-
---
+++ src/class/styles.js
@@ -0,0 +1,23 @@
+import { StyleSheet} from 'react-native';
+
+
+export const styles = StyleSheet.create({
+  container : {
+    flex: 1,
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "blue"
+  },
+  texto: {
+    fontSize: 100,
+    fontWeight: "bold",
+    color: 'white'
+  },
+   
+  paragraph:{
+    paddingTop:20,
+    fontSize:30,
+    color:"yellow",
+  },
+})
